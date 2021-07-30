0533a9f2da2b3754405ba21ace0f2688c28ec725
arunstop <arunstop@gmail.com>
2021-07-30 11:29:50 +0700

    ADDED snap layout auto queue

--- components/SnapLayout.vue/SnapLayoutInitializerApp.vue
+++ components/SnapLayout.vue/SnapLayoutInitializerApp.vue
@@ -1,5 +1,10 @@
 <template>
-    <v-btn class="transparent text-capitalize pa-2" height="100%" depressed @click="openApp(app)">
+  <v-btn
+    class="transparent text-capitalize pa-2"
+    height="100%"
+    depressed
+    @click="openSnapApp()"
+  >
     <div class="d-flex flex-column justify-center align-center">
       <v-icon size="30" :color="app.iconColor">{{ app.icon }}</v-icon>
       <h5 class="mt-2">{{ cutString20(app.title) }}</h5>
@@ -7,20 +12,31 @@
   </v-btn>
 </template>
 <script>
-import{ mapGetters} from 'vuex'
+import { mapGetters } from 'vuex'
 export default {
-    props: {
+  props: {
+    snap: { type: Object, default: () => ({}) },
     app: {
       type: Object,
       default: () => {},
     },
   },
-  computed:{
-      ...mapGetters('app', ['cutString20'])
-  },methods:{
-    openApp(app) {
-      this.$store.dispatch('app/window/openApp', app)
+  computed: {
+    ...mapGetters('app', ['cutString20']),
+  },
+  methods: {
+    openSnapApp() {
+    //     console.log({
+    //     type: this.snap.type,
+    //     index: this.snap.index,
+    //     app: this.app,
+    //   })
+      this.$store.dispatch('app/snap/addSnap', {
+        type: this.snap.type,
+        index: this.snap.index,
+        app: this.app,
+      })
     },
-  }
+  },
 }
 </script>
