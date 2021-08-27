b11aee7d9e271d23cb37e1ac03f33e47acc2c223
arunstop <arunstop@gmail.com>
2021-08-27 08:18:20 +0700

    ADDED sub context menu on desktop -> view

--- components/Desktop/DesktopContextMenu.vue
+++ components/Desktop/DesktopContextMenu.vue
@@ -5,43 +5,57 @@
         v-if="option.subItemList.length === 0"
         link
         class="rounded-lg"
+        :disabled="option.disabled"
       >
         <v-list-item-icon class="me-2">
-          <v-icon color="black">
+          <v-icon :color="option.disabled ? 'blue-grey-darken-1' : 'black'">
             {{ option.icon }}
           </v-icon>
         </v-list-item-icon>
-        <v-list-item-subtitle class="black--text">
-          {{ option.label }}
-        </v-list-item-subtitle>
+        <v-list-item-subtitle class="d-flex font-weight-regular align-center">
+                <span :class="option.disabled ? 'blue-grey--text text--darken-1' : 'black--text'">{{ option.label }}</span>
+                <v-spacer/>
+                <h5 class="font-weight-regular">{{ option.keyboard }}</h5>
+              </v-list-item-subtitle>
       </v-list-item>
       <v-menu v-else open-on-hover offset-x offset-overflow>
         <template #activator="{ on, attrs }">
           <v-list-item link class="rounded-lg" v-bind="attrs" v-on="on">
             <v-list-item-icon class="me-2">
-              <v-icon color="black">
+              <v-icon :color="option.disabled ? 'blue-grey-darken-1' : 'black'">
                 {{ option.icon }}
               </v-icon>
             </v-list-item-icon>
-            <v-list-item-subtitle class="black--text">
+            <v-list-item-subtitle class="black--text font-weight-regular">
               {{ option.label }}
             </v-list-item-subtitle>
             <v-list-item-icon class="me-2">
-              <v-icon color="black">
-                mdi-chevron-right
-              </v-icon>
+              <v-icon color="black"> mdi-chevron-right </v-icon>
             </v-list-item-icon>
           </v-list-item>
         </template>
-        <v-list  class="pa-0 blur-bg rounded-lg" dense>
-          <v-list-item
-            v-for="(subItem, index) in option.subItemList"
-            :key="index"
-            link
-            class="rounded-lg"
-          >
-            <v-list-item-subtitle>{{ subItem }}</v-list-item-subtitle>
-          </v-list-item>
+        <!-- sub menu -->
+        <v-list class="pa-0 blur-bg rounded-lg" dense>
+          <div v-for="(subOption, index) in option.subItemList" :key="index">
+            <v-list-item link class="rounded-lg" :disabled="subOption.disabled">
+              <v-list-item-icon class="me-2">
+                <v-icon v-if="subOption.active" color="black">
+                  mdi-circle-medium
+                </v-icon>
+              </v-list-item-icon>
+              <v-list-item-icon class="me-2">
+                <v-icon :color="subOption.disabled ? 'blue-grey-darken-1' : 'black'">
+                  {{ subOption.icon }}
+                </v-icon>
+              </v-list-item-icon>
+              <v-list-item-subtitle class="d-flex font-weight-regular align-center">
+                <span :class="subOption.disabled ? 'blue-grey--text text--darken-1' : 'black--text'">{{ subOption.label }}</span>
+                <v-spacer/>
+                <h5 class="font-weight-regular">{{ subOption.keyboard }}</h5>
+              </v-list-item-subtitle>
+            </v-list-item>
+            <v-divider v-if="subOption.underLined" />
+          </div>
         </v-list>
       </v-menu>
       <v-divider v-if="option.underLined" />
--- middleware/initDesktopOptionList.js
+++ middleware/initDesktopOptionList.js
@@ -1,28 +1,34 @@
 export default ({ store }) => {
     const desktopOptionList = [
         {
-            icon: "mdi-view-grid-outline", label: "View", subItemList: [
-                "1", "2"
-            ], underLined: false
+            icon: "mdi-view-grid-outline", label: "View", keyboard: "", disabled:false, underLined: false, subItemList: [
+                { icon: "mdi-checkbox-blank-outline", label: "Large icons", active: false, keyboard: "Ctrl + Shift + 2", disabled:false, underLined: false },
+                { icon: "mdi-grid-large", label: "Medium icons", active: true, keyboard: "Ctrl + Shift + 3", disabled:false, underLined: false },
+                { icon: "mdi-grid", label: "Small icons", active: false, keyboard: "Ctrl + Shift + 4", disabled:false, underLined: true },
+                { icon: "mdi-flash-auto", label: "Auto arrange icons", active: false, keyboard: "", disabled:true, underLined: false },
+                { icon: "mdi-file-table-box-outline mdi-flip-v", label: "Align icons to grid", active: false, keyboard: "", disabled:true, underLined: true },
+                { icon: "", label: "Show desktop icons", active: false, keyboard: "", disabled:false, underLined: false },
+            ]
         },
         {
-            icon: "mdi-swap-vertical", label: "Sort By", subItemList: [
+            icon: "mdi-swap-vertical", label: "Sort By", keyboard: "", disabled:false, underLined: false, subItemList: [
                 "1", "2"
-            ], underLined: true
+            ]
         },
-        { icon: "mdi-refresh", label: "Refresh", subItemList: [], underLined: false },
-        { icon: "", label: "Next desktop background", subItemList: [], underLined: false },
-        { icon: "", label: "Paste shortcut", subItemList: [], underLined: false },
-        { icon: "mdi-undo-variant mdi-rotate-315", label: "Undo Move", subItemList: [], underLined: false },
+        { icon: "mdi-refresh", label: "Refresh", keyboard: "F5", disabled:false, underLined: true, subItemList: [] },
+        { icon: "", label: "Next desktop background", keyboard: "", disabled:false, underLined: false, subItemList: [] },
+        { icon: "", label: "Paste shortcut", keyboard: "", disabled:true, underLined: false, subItemList: [] },
+        { icon: "mdi-undo-variant mdi-rotate-315", label: "Undo", keyboard: "Ctrl + Z", disabled:false, underLined: false, subItemList: [] },
+        { icon: "mdi-redo-variant mdi-rotate-45", label: "Redo", keyboard: "Ctrl + Y", disabled:false, underLined: false, subItemList: [] },
         {
-            icon: "mdi-plus-circle-outline", label: "New Item", subItemList: [
+            icon: "mdi-plus-circle-outline", label: "New Item", keyboard: "", disabled:false, underLined: true, subItemList: [
                 "1", "2"
-            ], underLined: true
+            ]
         },
-        { icon: "mdi-application-cog mdi-rotate-180", label: "Display settings", subItemList: [], underLined: false },
-        { icon: "mdi-pencil-outline", label: "Personalize", subItemList: [], underLined: true },
-        { icon: "mdi-console", label: "Open in Windows Terminal", subItemList: [], underLined: true },
-        { icon: "mdi-card-plus-outline", label: "Show more options", subItemList: [], underLined: true },
+        { icon: "mdi-application-cog mdi-rotate-180", label: "Display settings", keyboard: "", disabled:false, underLined: false, subItemList: [] },
+        { icon: "mdi-brush", label: "Personalize", keyboard: "", disabled:false, underLined: true, subItemList: [] },
+        { icon: "mdi-console", label: "Open in Windows Terminal", keyboard: "", disabled:false, underLined: true, subItemList: [] },
+        { icon: "mdi-card-plus-outline", label: "Show more options", keyboard: "Shift + F5", disabled:false, underLined: true, subItemList: [] },
     ]
 
     store.dispatch('desktop/initDesktopOptionList', desktopOptionList)
